7e8ba7490e2100a3a1634f7d1d0a67b75594a749
DucViet1021998 <viethg1021998@gmail.com>
2022-11-23 14:46:43 +0700

    update

--- src/components/Layout/ComponentsLayout/index.jsx
+++ src/components/Layout/ComponentsLayout/index.jsx
@@ -1,3 +1,33 @@
+import styles from './Header/Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import classNames from 'classnames/bind'
+import images from '~/assets/images'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+
+const cx = classNames.bind(styles)
+
 export default function Header() {
-    return <h1>Header</h1>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className="logo">
+                    <img width={'118px'} height={'42px'} src={images.logo} alt='tiktok' />
+                </div>
+                <div className={cx("search")}>
+                    <input placeholder='Search accounts and videos' spellCheck="fasle" />
+                    <FontAwesomeIcon className={cx("clear")} icon={faCircleXmark} />
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx("search-btn")}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx("actions")}>
+
+                </div>
+            </div>
+        </header>
+    )
+
 }
--- src/components/Layout/DefaultLayout/SideBar/index.jsx
+++ src/components/Layout/DefaultLayout/SideBar/index.jsx
@@ -1,3 +1,13 @@
+import styles from './SideBar.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 export default function SideBar() {
-    return <h2>SideBar</h2>
+    return (
+        <aside className={cx("wrapper")}>
+
+            <h2>SideBar</h2>
+
+        </aside>
+    )
 }
